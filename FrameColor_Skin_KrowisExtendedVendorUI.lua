a30c3a837a004cff6346d71ee207833b05daa8ef
Slothpala <slothpala@gmail.com>
2023-10-29 01:56:19 +0200

    changed recolor method to followModule

--- FrameColor_Skin_KrowisExtendedVendorUI.lua
+++ FrameColor_Skin_KrowisExtendedVendorUI.lua
@@ -1,9 +1,7 @@
 local info = {
     moduleName = "Krowi_ExtendedVendorUI",
-    color1 = {
-        name = "Frame",
-        desc = "",
-    },
+    moduleDesc = "This module has no settings. It follows the settings of the MerchantFrame module.",
+    followModule = "MerchantFrame",
 }
 
 local module = FrameColor_CreateSkinModule(info)
@@ -11,24 +9,15 @@ local module = FrameColor_CreateSkinModule(info)
 local SetDesaturation = SetDesaturation
 local SetVertexColor = SetVertexColor
 
-function module:OnEnable()
-    local main_color = self:GetColor1()
-    self:Recolor(main_color, 1)
-end
-
-function module:OnDisable()
-    local color = {r=1,g=1,b=1,a=1}
-    self:Recolor(color, 0)
-end
-
-function module:Recolor(main_color, desaturation)
+function module:Recolor(...)
+    local color, _, _, desaturation = ...
     for _ ,v in pairs({
         KrowiEVU_BottomExtensionLeftBorder,
         KrowiEVU_BottomExtensionMidBorder,
         KrowiEVU_BottomExtensionRightBorder,
     }) do 
         v:SetDesaturation(desaturation)
-        v:SetVertexColor(main_color.r, main_color.g, main_color.b)
+        v:SetVertexColor(color.r, color.g, color.b)
     end
 end
 
